4d6ea86b8acf4a78ab2b934aa6147e2fc0bd821d
ushie pius <preciousprogrammer2@gmail.com>
2024-10-13 04:05:58 +0100

    googlev2

--- src/app/search/error.tsx
+++ src/app/search/error.tsx
@@ -1,11 +1,8 @@
 "use client"
-import React, { useEffect } from 'react'
 
-const error = ({error, reset}:{error:any, reset: any}) => {
+const error = () => {
 
-    useEffect(()=>{
 
-    },[error])
   return (
     <div className='flex flex-col justify-center items-center pt-10'>
         <h1 className='text-3xl mb-4'>Something went wrong</h1> 
