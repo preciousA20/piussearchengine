5866980c6842699b6fe3a6d15c84d36d768402ea
ushie pius <preciousprogrammer2@gmail.com>
2024-10-13 03:51:37 +0100

    googlev2

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { RxCross2 } from 'react-icons/rx'
 import { AiOutlineSearch } from 'react-icons/ai'
 import { BsFillMicFill } from 'react-icons/bs'
