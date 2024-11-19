e28b087059935e2ba7774f6b9f553e9ee9093ece
ushie pius <preciousprogrammer2@gmail.com>
2024-11-19 04:49:30 +0100

    fix bugs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css"
 import Footer from "@/components/Footer"
-import { Suspense } from "react";
+import { Suspense } from "react"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
